cd5151241787d2a426ae400b0422b55e1d7da3c4
HubbeDev <privat@responsivhub.com>
2023-08-10 00:00:09 +0200

    Revert ":sparkles: Added fotter"
    This reverts commit 73ea7d3378841baf49171d3b7890b16991d2128a.

--- src/lib/components/Footer.svelte
+++
@@ -1,6 +0,0 @@
-<footer class=" px-4 mt-9">
-	<p class="scroll-m-20 text-lg font-normal text-muted-foreground tracking-tight text-center mx-auto">
-		Made with Sveltekit - Code can be found at
-		<a href="https://github.com/HubbeDev/HubbeDev" title="Github link">Github</a>
-	</p>
-</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-	import Header from '$components/Header.svelte';
-	import Footer from '$components/Footer.svelte';
 	import '../app.postcss';
+
+	import Header from '$components/Header.svelte';
 </script>
 
 <Header />
-<div class="container mx-auto max-w-3xl px-8 pt-2 flex flex-col">
+
+<div class="container mx-auto max-w-3xl px-8 pt-2 flex flex-col ">
+
 	<slot />
 </div>
-<Footer />
+
