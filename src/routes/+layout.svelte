73ea7d3378841baf49171d3b7890b16991d2128a
HubbeDev <privat@responsivhub.com>
2023-08-09 23:59:55 +0200

    :sparkles: Added fotter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script>
-	import '../app.postcss';
-
 	import Header from '$components/Header.svelte';
+	import Footer from '$components/Footer.svelte';
+	import '../app.postcss';
 </script>
 
 <Header />
-
-<div class="container mx-auto max-w-3xl px-8 pt-2 flex flex-col ">
-
+<div class="container mx-auto max-w-3xl px-8 pt-2 flex flex-col">
 	<slot />
 </div>
-
+<Footer />
