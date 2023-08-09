9013ed1556a1178c164e7691a994ca12e67084d0
HubbeDev <privat@responsivhub.com>
2023-08-10 00:46:39 +0200

    :sparkles: Hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,14 @@
 	console.log(data);
 </script>
 
+<section class="space-y-4 mb-8">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Hello! Im Hubbe.</h1>
+	<p class="text-xl text-muted-foreground text-balance md:pr-12">
+		Welcome to my personal blog about web design and web development! Here, I'll be sharing my
+		insights and tips on how to create beautiful and functional websites.
+	</p>
+</section>
+
 {#if data.posts.length}
 	{#each data.posts as post}
 		<PostItem {post} />
